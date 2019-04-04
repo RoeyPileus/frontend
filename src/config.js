6a04f4ec74040e35ed497a5837e943e61dcc7964
Roey Wegerhoff <roeywegerhoff@roeys-mbp.home>
2019-04-04 14:21:55 +0300

    initial commit

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-tutorial-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://a65ln14wa5.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_UQTSQcUgI",
+      APP_CLIENT_ID: "68psrqdkqs9i6a2bepe1nn4uto",
+      IDENTITY_POOL_ID: "us-east-1:27ced6fc-356d-46e6-988a-f04197ab68d4"
+    }
+  };
+  
